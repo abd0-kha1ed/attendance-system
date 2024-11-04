ae9b386c8163b526b5584fb2fa662719902d87e2
abdelrahman <bad606609@gmail.com>
2024-11-04 20:49:03 +0200

    ..

--- lib/feature/splash/presentation/view/widgets/splash_view_body.dart
+++ lib/feature/splash/presentation/view/widgets/splash_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:attendance/core/utils/assets.dart';
 import 'package:attendance/feature/home/presentation/view/home_view.dart';
+import 'package:attendance/feature/login/presentation/view/login_view.dart';
 import 'package:flutter/material.dart';
 
 class SplashViewBody extends StatefulWidget {
@@ -64,7 +65,7 @@ class _SplashViewBodyState extends State<SplashViewBody>
         context,
         MaterialPageRoute(
           builder: (context) {
-            return const HomeView();
+            return const LoginView();
           },
         ),
       );
