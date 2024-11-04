855395aa49192ecefc9d83803bade9c3500812e8
mahmoud ahmed <mahmoudahmedhsn@gmail>
2024-11-04 16:36:49 +0200

    .

--- lib/feature/home/presentation/view/widgets/custom_dropdown_button.dart
+++ lib/feature/home/presentation/view/widgets/custom_dropdown_button.dart
@@ -7,6 +7,7 @@ class CustomDropdownButton extends StatefulWidget {
   final List<String> items;
 
   @override
+  // ignore: library_private_types_in_public_api
   _CustomDropdownButtonState createState() => _CustomDropdownButtonState();
 }
 
