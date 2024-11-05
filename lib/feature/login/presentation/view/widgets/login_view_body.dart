b42da9ed0ac5ad9697d034d47489c4cd289dda0e
abdelrahman <bad606609@gmail.com>
2024-11-05 14:18:11 +0200

    show snack bar handling

--- lib/feature/login/presentation/view/widgets/login_view_body.dart
+++ lib/feature/login/presentation/view/widgets/login_view_body.dart
@@ -6,7 +6,6 @@ import 'package:attendance/core/widgets/custom_text_filed.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
 import 'package:go_router/go_router.dart';
 import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
@@ -23,9 +22,9 @@ class _LoginViewBodyState extends State<LoginViewBody> {
   String? email;
   String? passWord;
   bool isLoading = false;
-  // Initial selection for "Assistant"
   final GlobalKey<FormState> formkey = GlobalKey();
   AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+
   @override
   Widget build(BuildContext context) {
     return ModalProgressHUD(
@@ -37,143 +36,115 @@ class _LoginViewBodyState extends State<LoginViewBody> {
           autovalidateMode: autovalidateMode,
           child: ListView(
             children: [
-              const SizedBox(
-                height: 60,
-              ),
-              const Icon(
-                Icons.school,
-                size: 120,
-                color: kLogoColor,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
+              const SizedBox(height: 60),
+              const Icon(Icons.school, size: 120, color: kLogoColor),
+              const SizedBox(height: 20),
               const Center(
                 child: Text(
                   'Attendance System',
                   style: TextStyle(fontSize: 25, fontWeight: FontWeight.w700),
                 ),
               ),
-              const SizedBox(
-                height: 25,
-              ),
-              Center(
-                child: ToggleButtons(
-                  color: kLogoColor,
-                  borderColor: Colors.blue,
-                  selectedBorderColor: Colors.blue,
-                  selectedColor: Colors.white,
-                  fillColor: Colors.blue,
-                  borderRadius: BorderRadius.circular(28),
-                  isSelected: isSelected,
-                  onPressed: (int index) {
-                    setState(() {
-                      for (int i = 0; i < isSelected.length; i++) {
-                        isSelected[i] = i == index; // Update selection state
-                      }
-                    });
-                  },
-                  children: const [
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 15.0),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 5.0),
-                            child: Icon(
-                              Icons.help_center,
-                            ),
-                          ), // Assistant icon
-                          Text('Assistant'),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 15.0),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 5),
-                            child: Icon(Icons.school),
-                          ), // Teacher icon
-                          Text('Teacher'),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const SizedBox(
-                height: 25,
-              ),
+              const SizedBox(height: 25),
+              _buildRoleToggleButtons(),
+              const SizedBox(height: 25),
               CustomTextField(
-                onChanged: (value) {
-                  email = value;
-                },
+                onChanged: (value) => email = value,
                 labelText: 'Email',
               ),
-              const SizedBox(
-                height: 12,
-              ),
+              const SizedBox(height: 12),
               CustomTextField(
                 obscureText: _isObscured,
-                onChanged: (value) {
-                  passWord = value;
-                },
-                labelText: 'PassWord',
+                onChanged: (value) => passWord = value,
+                labelText: 'Password',
                 iconButton: IconButton(
-                  icon: Icon(
-                    _isObscured ? Icons.visibility_off : Icons.visibility,
-                  ),
-                  onPressed: () {
-                    setState(() {
-                      _isObscured = !_isObscured; // Toggle password visibility
-                    });
-                  },
+                  icon: Icon(_isObscured ? Icons.visibility_off : Icons.visibility),
+                  onPressed: () => setState(() => _isObscured = !_isObscured),
                 ),
               ),
-              const SizedBox(
-                height: 16,
-              ),
+              const SizedBox(height: 16),
               CustomContainer(
-                onTap: () async {
-                  if (formkey.currentState!.validate()) {
-                    // formkey.currentState!.save();
-                    isLoading = true;
-
-                    setState(() {});
-                    try {
-                      final credential = await FirebaseAuth.instance
-                          .signInWithEmailAndPassword(
-                              email: email!, password: passWord!);
-                      GoRouter.of(context).push(AppRouters.kHomeView);
-                    } on FirebaseAuthException catch (e) {
-                      if (e.code == 'user-not-found') {
-                        print('No user found for that email.');
-                      } else if (e.code == 'wrong-password') {
-                        // print('Wrong password provided for that user.');
-                        showSnackBar(context, e.toString());
-                      }
-                    } catch (ex) {
-                      print(ex);
-                      showSnackBar(context, 'there was an error');
-                    }
-                    isLoading = false;
-                    setState(() {});
-                  } else {
-                    autovalidateMode = AutovalidateMode.always;
-                    setState(() {});
-                  }
-                },
+                onTap: _login,
                 text: 'Login',
-              )
+              ),
             ],
           ),
         ),
       ),
     );
   }
+
+  Widget _buildRoleToggleButtons() {
+    return Center(
+      child: ToggleButtons(
+        color: kLogoColor,
+        borderColor: Colors.blue,
+        selectedBorderColor: Colors.blue,
+        selectedColor: Colors.white,
+        fillColor: Colors.blue,
+        borderRadius: BorderRadius.circular(28),
+        isSelected: isSelected,
+        onPressed: (int index) {
+          setState(() {
+            for (int i = 0; i < isSelected.length; i++) {
+              isSelected[i] = i == index;
+            }
+          });
+        },
+        children: const [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15.0),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(Icons.help_center, size: 20),
+                SizedBox(width: 5),
+                Text('Assistant'),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 15.0),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(Icons.school, size: 20),
+                SizedBox(width: 5),
+                Text('Teacher'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _login() async {
+    if (formkey.currentState!.validate()) {
+      setState(() => isLoading = true);
+      try {
+        await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: email!,
+          password: passWord!,
+        );
+        GoRouter.of(context).push(AppRouters.kHomeView);
+      } on FirebaseAuthException catch (e) {
+        String errorMessage;
+        if (e.code == 'user-not-found') {
+          errorMessage = 'No user found with this email.';
+        } else if (e.code == 'wrong-password') {
+          errorMessage = 'Incorrect password. Please try again.';
+        } else {
+          errorMessage = 'An unexpected error occurred.';
+        }
+        showSnackBar(context, errorMessage); // Display the exact error message
+      } catch (_) {
+        showSnackBar(context, 'An error occurred. Please try again.');
+      } finally {
+        setState(() => isLoading = false);
+      }
+    } else {
+      setState(() => autovalidateMode = AutovalidateMode.always);
+    }
+  }
 }
-//l;k
