c34842e390be793761620a39e5013e74b5192293
mahmoud ahmed <mahmoudahmedhsn@gmail>
2024-11-06 05:45:37 +0200

    ...

--- lib/feature/teacher/presentation/manger/add_new_assistant_cubit/cubit/add_new_assistan_cubit.dart
+++ lib/feature/teacher/presentation/manger/add_new_assistant_cubit/cubit/add_new_assistan_cubit.dart
@@ -3,7 +3,6 @@ import 'package:attendance/core/utils/firebase_services.dart';
 // ignore: depend_on_referenced_packages
 import 'package:bloc/bloc.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:go_router/go_router.dart';
 
 part 'add_new_assistan_state.dart';
 
